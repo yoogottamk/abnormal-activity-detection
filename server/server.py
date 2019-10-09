bfd646e0c693c97c1629ccf7b55f495e45b70a1a
YoogottamK <yoogottamk@outlook.com>
2019-10-09 22:24:47 +0530

    This works

--- server/server.py
+++ server/server.py
@@ -3,11 +3,15 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+f = open("out", "a+")
+
+@app.route("/", methods = ["POST"])
 def default():
+    f.write(request.data.decode("utf-8"))
+    f.flush()
     val = [ int(x) for x in request.data.decode("utf-8")[:-1].split(",") ]
-    return "TEST"
-    return str(np.average(np.array(val, np.uint8)))
+    print(str(np.average(np.array(val, np.uint16))))
+    return "Yay"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=9999)
