f15c611fa8be888a4398015736b2447208199494
GaurangTandon <1gaurangtandon@gmail.com>
2019-10-09 23:48:21 +0530

    fixed code; outputs are awesome :); beta release

--- checker.cpp
+++ checker.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 #include <set>
 #include <vector>
-
+#define min(x, y) ((x) >= (y) ? (y) : (x))
 using namespace std;
 using namespace __gnu_pbds;
 
@@ -12,72 +12,75 @@ typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag,
              tree_order_statistics_node_update>
     map_t;
 
-deque<long long> running_values(0);  // use deque here
-map_t running_values_set;
+deque<long long> runningValues(0);  // use deque here
+map_t runningValuesSet;
 const int WINDOW_SIZE = 100;
 const int SUB_WINDOW_SIZE = WINDOW_SIZE * 0.2;
-const int THRESHOLD = 75;
-long long running_sum = 0;
+const int THRESHOLD_PERCENT = 30;
+long long runningSum = 0;
 bool calibrated = false;
 deque<bool> wasAnomalyMeasured;
 
 long double averageWindow() {
-    return running_sum / (long double)WINDOW_SIZE;
+    return runningSum / (long double)WINDOW_SIZE;
 }
 
 // needs to called at every time instant
 bool isAnomaly(long long value) {
     long long avg = (long long)averageWindow(),
-              maxLimit = (long long)((1 + THRESHOLD / 100.0) * avg),
-              minLimit = (long long)((1 - THRESHOLD / 100.0) * avg);
+              maxLimit = (long long)((1 + THRESHOLD_PERCENT / 100.0) * avg),
+              minLimit = (long long)((1 - THRESHOLD_PERCENT / 100.0) * avg);
 
     return (value >= maxLimit || value <= minLimit);
 }
 
 void takeInput(long long value) {
-    if ((int)(running_values.size()) == WINDOW_SIZE) {
-        long long last = running_values[0];
-        running_sum -= last;
+    while ((int)(runningValues.size()) >= WINDOW_SIZE) {
+        long long last = runningValues[0];
+        runningSum -= last;
         wasAnomalyMeasured.pop_front();
-        running_values.pop_front();
-        running_values_set.erase(running_values_set.upper_bound(last));
+        runningValues.pop_front();
+        runningValuesSet.erase(runningValuesSet.upper_bound(last));
     }
 
-    running_sum += value;
-    running_values.push_back(value);
-    running_values_set.insert(value);
+    runningSum += value;
+    runningValues.push_back(value);
+    runningValuesSet.insert(value);
+
     if (calibrated)
         wasAnomalyMeasured.push_back(isAnomaly(value));
     else
         wasAnomalyMeasured.push_back(false);
 }
+long long counter = 0;
 
 void sendResponse() {
-    int anomalousCount = 0;
-    for (int i = 0; i < SUB_WINDOW_SIZE; i++) {
+    int anomalousCount = 0,
+        lim = min((int)wasAnomalyMeasured.size(), SUB_WINDOW_SIZE);
+
+    for (int i = 0; i < lim; i++) {
         anomalousCount += wasAnomalyMeasured[i];
     }
 
-    int percentage = anomalousCount / (long double)SUB_WINDOW_SIZE;
+    int percentage = anomalousCount * 100 / (long double)lim;
 
-    if (percentage >= THRESHOLD)
+    if (percentage >= THRESHOLD_PERCENT)
         printf("1\n");
-    else
+    else {
         printf("0\n");
+    }
     fflush(stdout);
 }
 
 int main() {
     long long val;
-    long long counter = 0;
 
-    while (scanf("%lld", &val)) {
+    while (scanf("%lld ", &val) != EOF) {
         takeInput(val);
         sendResponse();
 
-        if (!calibrated && counter < WINDOW_SIZE)
-            counter++;
-        else
+        counter++;
+        if (counter > WINDOW_SIZE)
             calibrated = true;
     }
 }
