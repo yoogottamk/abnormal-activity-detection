5ecf6ea6bdda052055776dd5ea34cd6263faf682
YoogottamK <yoogottamk@outlook.com>
2019-10-10 09:20:15 +0530

    Server code updated

--- server/server.py
+++ server/server.py
@@ -1,15 +1,22 @@
 from flask import Flask, request
+from invoker import start, read, write
 
 app = Flask(__name__)
 
 f = open("out", "a+")
 
+process = start("./checker")
+
 @app.route("/", methods = ["POST"])
 def default():
     f.write(request.data.decode("utf-8"))
     f.flush()
-    val = [ int(x) for x in request.data.decode("utf-8").split("\n") if len(x) > 0 ]
 
+    write(process, request.data.decode("utf-8"))
+    response = read(process)
+    write(process, "-1")
+
+    return "1" if response.find("1") != -1 else "0"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=9999)
