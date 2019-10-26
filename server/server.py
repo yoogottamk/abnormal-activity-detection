dd6ad747dcbadbca13fb9b5e7173aaeefd7707e8
YoogottamK <yoogottamk@outlook.com>
2019-10-26 17:16:48 +0530

    No guru again!

--- server/server.py
+++ server/server.py
@@ -7,8 +7,11 @@ f = open("out", "a+")
 
 process = start("./checker")
 
-@app.route("/", methods = ["POST"])
+@app.route("/", methods=["POST"])
 def default():
+    response = request.data.decode("utf-8")
+    return "0"
+
     f.write(request.data.decode("utf-8"))
     f.flush()
 
@@ -16,10 +19,16 @@ def default():
     response = read(process)
     write(process, "-1")
 
+    print(request.data.decode("utf-8"))
+    print(response)
 
     x = "1" if response.find("1") != -1 else "0"
     print("Sending ", x)
     return x
+
+@app.route("/test/", methods=["POST", "GET"])
+def test():
+    return "WHOO"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=9999)
