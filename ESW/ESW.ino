76eca4d42d48eb260f06b88a113ddd7fce568ca9
YoogottamK <yoogottamk@outlook.com>
2019-11-02 19:31:46 +0530

    this works

--- ESW/ESW.ino
+++ ESW/ESW.ino
@@ -4,41 +4,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-String urlencode(String str)
-{
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    char code2;
-    for (int i =0; i < str.length(); i++){
-      c=str.charAt(i);
-      if (c == ' '){
-        encodedString+= '+';
-      } else if (isalnum(c)){
-        encodedString+=c;
-      } else{
-        code1=(c & 0xf)+'0';
-        if ((c & 0xf) >9){
-            code1=(c & 0xf) - 10 + 'A';
-        }
-        c=(c>>4)&0xf;
-        code0=c+'0';
-        if (c > 9){
-            code0=c - 10 + 'A';
-        }
-        code2='\0';
-        encodedString+='%';
-        encodedString+=code0;
-        encodedString+=code1;
-        //encodedString+=code2;
-      }
-      yield();
-    }
-    return encodedString;
-    
-}
-
 #define sz 3000
 short arr[sz];
 int c=0;
@@ -100,7 +65,7 @@ void setup() {
      * TODO: Use WiFiMulti maybe
      */
     Serial.print("Connecting to WiFi");
-    WiFi.begin("yoogottam", "_plis_/\\_plis_");
+    WiFi.begin("yoogottam", "plis_/\\_plis");
 
     while(WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -114,7 +79,7 @@ void setup() {
 
 String serverResponse;
 // please put forward slash in the ending
-String serverURL = "http://25b4fcde.ngrok.io/test/";
+String serverURL = "http://223cc4f0.ngrok.io/";
 char* successMsg = "SUCCESS: ";
 void sendString(String str) {
     Serial.println("Sending data: ");
@@ -125,7 +90,7 @@ void sendString(String str) {
 
 //    str = "data=" + urlencode(str);
     http.begin(serverURL);
-    int retCode = http.GET();
+    int retCode = http.POST(str);
 
     if(retCode > 0) {
         Serial.print(successMsg);
