e227d2287c782f5f01ca9f17cc242cb75c951e9c
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2019-11-08 13:11:22 +0530

    Further setup Why does print not happen :/

--- server/static/js/plot.js
+++ server/static/js/plot.js
@@ -1,9 +1,16 @@
 window.plot = function (inData, outData) {
     const ctx = document.getElementById('myChart').getContext('2d');
+    const labels = [];
+    for (let i = 1, len = inData.length; i <= len; i++) {
+        labels.push(i);
+    }
 
+    console.log(inData);
+    console.log(outData);
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
+            labels,
             datasets: [{
                 label: 'ESP data',
                 data: [inData],
