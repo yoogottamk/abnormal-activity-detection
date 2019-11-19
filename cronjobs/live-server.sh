4090ede89d7b0772be42cd2e0d0bfd516098922f
yoogottamk <yoogottamk@outlook.com>
2019-11-19 13:25:20 +0530

    oops

--- cronjobs/live-server.sh
+++ cronjobs/live-server.sh
@@ -7,7 +7,7 @@ BOT_URL='https://api.telegram.org/bot853098987:AAF87AC_3qzruYsN9Cjpk6aKilK5Om9RJ
 FILE_DOWN="${HOMEDIR}SERVER_DOWN"
 
 if [ ! -e "$FILE_DOWN" ]; then
-	if ! wget "localhost:9999/test/"; then
+	if ! wget "localhost:9999/test/" >/dev/null 2>&1; then
 	    for id in ${CHAT_IDs[@]}; do
 		wget -O- --header="Content-Type: application/json" --post-data="{\"chat_id\": \"$id\", \"parse_mode\": \"Markdown\", \"text\": \"*ALERT!* The Flask server seems to be down.\"}" $BOT_URL > /dev/null 2>&1
 	    done
