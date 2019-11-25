5d8286f5ba00b79a4478f9676dfa4abf60fb4fd1
yoogottamk <yoogottamk@outlook.com>
2019-11-25 16:55:19 +0530

    WIP

--- server/log.py
+++ server/log.py
@@ -1,9 +1,17 @@
 import sys
 import re
+import datetime as dt
+import os
+import numpy as np
 
 from invoker import start, terminate
 
+VALUES_PER_SECOND = 1200
+
 def getCheckerOutputFromLog(logFile):
+    if not os.path.exists(logFile):
+        return ""
+
     with open(logFile, "r") as f:
         text = f.read()
 
@@ -13,4 +21,20 @@ def getCheckerOutputFromLog(logFile):
     output, err = proc.communicate((''.join(vals) + " -1\n").encode())
     terminate(proc)
 
-    return output.decode("utf-8")
+    out = np.array(list(output.decode("utf-8")))
+
+    splits = np.array_split(out, VALUES_PER_SECOND)
+
+    output = []
+
+    for split in splits:
+        output.append('1' if '1' in split else '0')
+
+    return ''.join(output)
+
+
+def getHourLog(year,month,date,hour):
+    return getCheckerOutputFromLog(f"../data/${year}-${month}-${date}_${hour}.log")
+
+def getDayLog(year,month,date):
+    pass
