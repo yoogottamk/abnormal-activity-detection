80db40c0d5366dd048bb20ea8adcce6e29b655b0
yoogottamk <yoogottamk@outlook.com>
2019-11-25 23:45:36 +0530

    trying to plot

--- server/static/js/plot.js
+++ server/static/js/plot.js
@@ -5,10 +5,11 @@ function round(x) {
 
 const normal = "rgba(74,192,192,1)", red = "rgba(200,50,50,1)";
 window.plot = function (inData, outData, step) {
-    const ctx = document.getElementById('myChart').getContext('2d');
+    const canvas = document.getElementById("myChart");
+    const ctx = canvas.getContext('2d');
     const labels = [];
     step = round(step);
-    let c = 0;
+let c = 0;
 
     for (let i = 1, len = inData.length; i <= len; i++) {
         labels.push(round(c));
@@ -22,64 +23,70 @@ window.plot = function (inData, outData, step) {
 
     let shouldColorRed = outData.indexOf(1) != -1;
 
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels,
-            datasets: [{
-                label: "Sound intensity for last ten seconds",
-                /* TODO make it red too! */
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: inData
-            }]
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    new Chart(ctx).Line({
+        labels,
+        datasets: [{
+            label: "Sound intensity for last ten seconds",
+            /* TODO make it red too! */
+            borderColor: shouldColorRed ? red : normal,
+            /*
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        */
+            fill: false,
+            data: inData
+        }]
+    }/*,
+        {
+        responsive: true,
+            /*
+        scaleLineColor: "rgba(0,0,0,.2)",
+        segmentShowStroke: false,
+
+
+        animation: {
+        duration: 0,
         },
-        options: {
-            animation: {
-                duration: 1000,
-            },
-            scaleLineColor: "rgba(0,0,0,.2)",
-            scaleGridLineColor: "rgba(0,0,0,.05)",
-            scaleFontColor: "#c5c7cc",
-            responsive: true,
-            title: {
-                display: true,
-                text: 'ESP data'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Seconds'
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        max: 4095,
-                        min: 0
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Value (volts)'
-                    }
-                }]
+        title: {
+        display: true,
+        text: 'ESP data'
+        },
+        tooltips: {
+        mode: 'index',
+        intersect: false,
+        },
+        hover: {
+        mode: 'nearest',
+        intersect: true
+        },
+
+        scales: {
+        xAxes: [{
+            display: true,
+            scaleLabel: {
+            display: true,
+            labelString: 'Seconds'
             }
+        }],
+        yAxes: [{
+            display: true,
+            ticks: {
+            max: 4095,
+            min: 0
+            },
+            scaleLabel: {
+            display: true,
+            labelString: 'Value (volts)'
+            }
+        }]
         }
-    });
+    }*/
+        );
 };
 
 const REFRESH_INTERVAL = 500;
@@ -97,6 +104,7 @@ function query(step) {
 window.processData = function (inString, outString, step, AUTO_RELOAD_ENABLED) {
     const inData = inString.split(" ").map(x => parseInt(x) * 10 + Math.floor(Math.random() * 10));
     const outData = outString.split(" ").map(x => parseInt(x));
+    console.log("plotting", inData, outData);
 
     window.plot(inData, outData, step);
 
