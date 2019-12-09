4bab48cfe6b89c537fe3e6aafe30e596e2a99131
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2019-12-09 18:44:51 +0530

    hide secrets UNTESTED

---
+++ cronjobs/sendMessage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# the textual message should be supplied as the first argument
+
+# convert string into array of strings
+read -r CHAT_IDs < "${HOMEDIR}secretdata/CHAT_IDS"
+CHAT_IDs=($CHAT_IDs) # https://stackoverflow.com/a/9294015
+
+BOTUID=(cat "${HOMEDIR}secretdata/BOTUID")
+BOT_URL="https://api.telegram.org/${BOTUID}/sendMessage"
+
+for id in ${CHAT_IDs[@]}; do
+    wget -O- --header="Content-Type: application/json" --post-data="{\"chat_id\": \"$id\", \"parse_mode\": \"Markdown\", \"text\": \"$1\"}" $BOT_URL > /dev/null 2>&1
+done
--- server/bot.py
+++ server/bot.py
@@ -1,13 +1,23 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
 import matplotlib
 matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 SEP = " "
 
-CHAT_IDs = ["630804492", "879466464", "623858814"]
-BOT_URL = 'https://api.telegram.org/bot853098987:AAF87AC_3qzruYsN9Cjpk6aKilK5Om9RJu4/'
+with open("secretdata/BOTUID") as f:
+    BOT_UID = f.read().trim()
+
+BOT_URL = f'https://api.telegram.org/{BOT_UID}/'
+
+with open("secretdata/CHAT_IDS") as f:
+    CHAT_IDs = f.read().trim()
+    if CHAT_IDs:
+        CHAT_IDs = CHAT_IDs.split(" ")
+    else:
+        CHAT_IDs = []
+
 
 def send_text(msg):
     url = BOT_URL + 'sendMessage'
@@ -15,18 +25,19 @@ def send_text(msg):
     for chat_id in CHAT_IDs:
         requests.post(url, json={"chat_id": chat_id, "text": msg})
 
+
 def send_graph(val, detect, caption):
     url = BOT_URL + 'sendPhoto'
 
     val = val[:val.find("-1")]
-    
-    values = [ 10*int(x) for x in val.strip().split(SEP) ]
-    detected_at = np.array([ int(x) for x in list(detect.strip()) ])
+
+    values = [10*int(x) for x in val.strip().split(SEP)]
+    detected_at = np.array([int(x) for x in list(detect.strip())])
     indices = np.where(detected_at == 1)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_ylim([0,4095])
+    ax.set_ylim([0, 4095])
     ax.plot(values)
 
     for idx in indices[0]:
@@ -35,5 +46,5 @@ def send_graph(val, detect, caption):
     fig.savefig("graph.png")
 
     for chat_id in CHAT_IDs:
-        requests.post(url, data={"chat_id": chat_id, "caption": caption}, files={"photo": open("./graph.png", "rb")})
-
+        requests.post(url, data={"chat_id": chat_id, "caption": caption}, files={
+                      "photo": open("./graph.png", "rb")})
